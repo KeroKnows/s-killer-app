39c6f031c35744765dc2d859d99dca369564f745
Sunny <shensunny594@gmail.com>
2021-10-23 19:04:48 +0800

    refactor: delete unnecessary variables

--- lib/jobs_info.rb
+++ lib/jobs_info.rb
@@ -1,6 +1,8 @@
 require 'http'
 require 'yaml'
 
+config = YAML.safe_load(File.read('../config/secrets.yml'))
+
 def get_searchAPI_url(job_title)
     url = "https://www.reed.co.uk/api/1.0/search?keywords=#{job_title}"
 end
@@ -9,34 +11,31 @@ def get_detailsAPI_url(job_id)
     url = "https://www.reed.co.uk/api/1.0/jobs/#{job_id}"
 end
 
-def send_request(url)
-    config = YAML.safe_load(File.read('../config/secrets.yml'))
+def send_request(config, url)
     HTTP.basic_auth(:user => "#{config['API_KEY']}", :pass => "").get(url)
 end
 
-reed_search_response = {}
-reed_details_response = {}
-
-reed_search_results = {}
-reed_details_results = {}
-
+api_response = {}
 job_results = {}
 
+# Happy request
 search_url = get_searchAPI_url("engineer")
-
-reed_search_response[search_url] = send_request(search_url)
-job_list = reed_search_response[search_url].parse['results']
+api_response[search_url] = send_request(config, search_url)
+job_list = api_response[search_url].parse['results']
 
 for job in job_list[1..10]
     jobID = job["jobId"]
-
     details_url = get_detailsAPI_url(jobID)
-    reed_details_response[details_url] = send_request(details_url)
-    reed_details_results[details_url] = reed_details_response[details_url].parse
+    
+    api_response[details_url] = send_request(config, details_url)
+    details_result = api_response[details_url].parse
 
     job_results[jobID] = {}
     job_results[jobID]["jobTitle"] = job["jobTitle"]
-    job_results[jobID]["jobDescription"] = reed_details_results[details_url]["jobDescription"]
+    job_results[jobID]["jobDescription"] = details_result["jobDescription"]
 end
 
 File.write('../spec/fixtures/job_results.yml', job_results.to_yaml)
+
+# Bad request
+api_response["no_auth"] = send_request({}, search_url)
