35620a1506af4e8aafca764598a92c141f870b6a
Sunny <shensunny594@gmail.com>
2021-10-23 17:56:31 +0800

    feat: request reed details api success

--- lib/jobs_info.rb
+++ lib/jobs_info.rb
@@ -1,18 +1,33 @@
 require 'http'
 require 'yaml'
 
-config = YAML.safe_load(File.read('../config/secrets.yml'))
+def get_searchAPI_url(job_title)
+    url = "https://www.reed.co.uk/api/1.0/search?keywords=#{job_title}"
+end
 
-job_title = "engineer"
-url = "https://www.reed.co.uk/api/1.0/search?keywords=#{job_title}"
+def get_detailsAPI_url(job_id)
+    url = "https://www.reed.co.uk/api/1.0/jobs/#{job_id}"
+end
 
+def send_request(url)
+    config = YAML.safe_load(File.read('../config/secrets.yml'))
+    HTTP.basic_auth(:user => "#{config['API_KEY']}", :pass => "").get(url)
+end
 
+reed_search_response = {}
+reed_details_response = {}
+reed_search_results = {}
+reed_details_results = {}
 
-response = HTTP.basic_auth(:user => "#{config['API_KEY']}", :pass => "").get(url)
-result = response.parse
+url = get_searchAPI_url("engineer")
 
-reed_response = {}
-reed_results = {}
+reed_search_response[url] = send_request(url)
+reed_search_results[url] = reed_search_response[url].parse
 
-reed_response[url] = response
-reed_results[url] = result
+puts reed_search_results[url]['results'][0]
+
+url = get_detailsAPI_url("44413283")
+reed_details_response[url] = send_request(url)
+reed_details_results[url] = reed_details_response[url].parse
+
+puts reed_details_results[url]["jobDescription"]
