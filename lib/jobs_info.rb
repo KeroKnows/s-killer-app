4b5ab435e166dd8000ab94adcf12e9d6271a9f2f
Sunny <shensunny594@gmail.com>
2021-10-23 19:45:25 +0800

    style: apply rubocop

--- lib/jobs_info.rb
+++ lib/jobs_info.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
+
 require 'http'
 require 'yaml'
 
 config = YAML.safe_load(File.read('../config/secrets.yml'))
 
-def get_searchAPI_url(job_title)
-    "https://www.reed.co.uk/api/1.0/search?keywords=#{job_title}"
+def get_searchapi_url(job_title)
+  "https://www.reed.co.uk/api/1.0/search?keywords=#{job_title}"
 end
 
-def get_detailsAPI_url(job_id)
-    "https://www.reed.co.uk/api/1.0/jobs/#{job_id}"
+def get_detailsapi_url(job_id)
+  "https://www.reed.co.uk/api/1.0/jobs/#{job_id}"
 end
 
 def send_request(config, url)
-    HTTP.basic_auth(:user => "#{config['API_KEY']}", :pass => "").get(url)
+  HTTP.basic_auth(user: "#{config['API_KEY']}", pass: '').get(url)
 end
 
 def get_detail_jd(config, job_results, api_response, job)
-    job_id = job["jobId"]
+  job_id = job['jobId']
 
-    # send request to details api
-    details_url = get_detailsAPI_url(job_id)
-    api_response[details_url] = send_request(config, details_url)
-    details_result = api_response[details_url].parse
+  # send request to details api
+  details_url = get_detailsapi_url(job_id)
+  api_response[details_url] = send_request(config, details_url)
+  details_result = api_response[details_url].parse
 
-    # get needed information
-    job_results[job_id] = {
-        "jobTitle": job["jobTitle"],
-        "jobDescription": details_result["jobDescription"]
-    }
+  # get needed information
+  job_results[job_id] = {
+    "jobTitle": job['jobTitle'],
+    "jobDescription": details_result['jobDescription']
+  }
 end
 
 api_response = {}
 job_results = {}
 
 # Happy request
-search_url = get_searchAPI_url("engineer")
+search_url = get_searchapi_url('engineer')
 api_response[search_url] = send_request(config, search_url)
 job_list = api_response[search_url].parse['results']
 
 # request only 5 jobs for explore usage
-job_list[1..5].map{ |job| get_detail_jd(config, job_results, api_response, job) }
+job_list[1..5].map { |job| get_detail_jd(config, job_results, api_response, job) }
 
 File.write('../spec/fixtures/job_results.yml', job_results.to_yaml)
 
 # Bad request
-api_response["no_auth"] = send_request({}, search_url)
+api_response['no_auth'] = send_request({}, search_url)
