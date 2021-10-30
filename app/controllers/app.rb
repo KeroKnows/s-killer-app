83203b230b82f108e50b9467b1edb5dfc874b402
joanna <joannawu@nlplab.cc>
2021-10-30 18:45:16 +0800

    feat: job-detail page

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,17 +2,45 @@
 
 require 'roda'
 require 'slim'
+require 'yaml' # develop
 
 module Skiller
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/views'
     plugin :halt
 
-    route do |routing| 
-
+    route do |router| 
       # GET /
-      routing.root do
+      router.root do
         view 'index'
+      end
+
+      # GET /index
+      router.on 'index' do
+        router.is do
+          view 'index'
+        end
+      end
+
+      router.on 'details' do
+        router.is do
+          # GET /details?query=[query]
+          router.get do
+            router.halt 400 unless router.params.include? 'query'
+
+            query = router.params['query']
+            # jobs = Reed::ReedApi.new(REED_TOKEN).job_list(query)
+            jobs = YAML.safe_load(File.read('spec/fixtures/job_lists.yml'))
+
+            # data = [{
+            #   'query' => query,
+            #   'title' => jobs.first['title'],
+            #   'description' => jobs.first['description']
+            # }]
+            view 'details', locals: { query: query, jobs: jobs }
+          end
+        end
+        
       end
     end
   end
