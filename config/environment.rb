9a709bb717656a664085636914ed8cca4f1e9555
Sunny <shensunny594@gmail.com>
2021-11-03 19:20:35 +0800

    chore: setup rake and database environment

--- config/environment.rb
+++ config/environment.rb
@@ -11,18 +11,28 @@ module Skiller
   class App < Roda
     plugin :environments
 
-    Figaro.application = Figaro::Application.new(
-      environment: environment,
-      path: File.expand_path('config/secrets.yml')
-    )
-    Figaro.load
+    configure do
+      # Environment variables setup
+      Figaro.application = Figaro::Application.new(
+        environment: environment,
+        path: File.expand_path('config/secrets.yml')
+      )
+      Figaro.load
+  
+      # Make the environment variables accessible
+      def self.config()
+        Figaro.env
+      end
 
-    # Make the environment variables accessible
-    def self.config()
-      Figaro.env
+      configure :development, :test do
+        ENV['DATABASE_URL'] = "sqlite://#{config.DB_FILENAME}"
+      end
+
+      # Database Setup
+      DB = Sequel.connect(ENV['DATABASE_URL'])
+      def self.DB()
+        DB
+      end
     end
-
-    # CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-    # REED_TOKEN = CONFIG['REED_TOKEN']
   end
 end
