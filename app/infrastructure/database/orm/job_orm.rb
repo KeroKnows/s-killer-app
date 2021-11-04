d85ac7227bbd09415c9e03e6d6785fea2c48beab
Sunny <shensunny594@gmail.com>
2021-11-04 08:25:58 +0800

    chore: timestamps for ORM

--- app/infrastructure/database/orm/job_orm.rb
+++ app/infrastructure/database/orm/job_orm.rb
@@ -1,8 +1,12 @@
+# http://sequel.jeremyevans.net/rdoc-plugins/classes/Sequel/Plugins/Timestamps.html
+
 module Skiller
   module Database
     # Object Relational Mapper for Job and PartialJob Entities
     class JobOrm < Sequel::Model(:jobs)
-      
+
+      # setting the update timestamp when creating
+      plugin :timestamps, update_on_create: true
     end
   end
 end
