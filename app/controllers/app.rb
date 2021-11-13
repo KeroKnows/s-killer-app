96fd1182bb804f130d0e6d0282da840797bfbe29
Simon Lee <simon2020291@gmail.com>
2021-11-14 01:52:41 +0800

    feat: retrieve jobs from database if the query has been searched

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -31,7 +31,8 @@ module Skiller
             router.halt 400 unless router.params.include? 'query'
 
             query = router.params['query']
-            jobs = Reed::PartialJobMapper.new(App.config).job_list(query)
+
+            jobs = JobCollector.new(App.config).jobs(query)
 
             view 'details', locals: { query: query, jobs: jobs }
           end
@@ -39,6 +40,29 @@ module Skiller
       end
     end
 
+    # request jobs using API if the query has not saved to database
+    class JobCollector
+      def initialize(config)
+        @partial_job_mapper = Skiller::Reed::PartialJobMapper.new(config)
+        @job_mapper = Skiller::Reed::JobMapper.new(config)
+      end
+
+      def jobs(query)
+        if Repository::QueriesJobs.query_exist?(query)
+          Repository::QueriesJobs.find_jobs_by_query(query)
+        else
+          request_jobs_and_update_database(query)
+        end
+      end
+
+      def request_jobs_and_update_database(query)
+        partial_jobs = @partial_job_mapper.job_list(query)
+        jobs = partial_jobs[0...10].map { |pj| Repository::Jobs.create(@job_mapper.job(pj.job_id)) }
+        jobs = jobs.map { |job| Repository::Jobs.create(job) }
+        Repository::QueriesJobs.create(query, jobs.map(&:db_id))
+        jobs
+      end
+    end
     # puts config.DB_FILENAME
   end
 end
