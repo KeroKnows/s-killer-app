664b687a1f2b0f1dff35709294b10a008b6dea57
Simon Lee <simon2020291@gmail.com>
2021-11-14 13:01:59 +0800

    feat: add `Repository::JobsSkills.create()`

--- app/domain/skills/repositories/jobs_skills.rb
+++ app/domain/skills/repositories/jobs_skills.rb
@@ -2,6 +2,7 @@
 
 module Skiller
   module Repository
+    # Provide the access to jobs_skills table via `JobSkillOrm`
     class JobsSkills
       def self.find_skills_by_job(job_db_id)
         Database::JobSkillOrm.where(job_db_id: job_db_id).all.map do |job_skill|
@@ -14,13 +15,19 @@ module Skiller
 
         job = Jobs.rebuild_entity(job_skill.job)
         Entity::Skill.new(
-          id:  job_skill.id,
+          id: job_skill.id,
           name: job_skill.skill,
           job_db_id: job_skill.job_db_id,
           salary: job.salary
         )
       end
 
+      def self.create(skills)
+        skills.map do |skill|
+          job_skill = Database::JobSkillOrm.create(job_db_id: skill.job_db_id, skill: skill.name)
+          rebuild_entity(job_skill)
+        end
+      end
     end
   end
 end
