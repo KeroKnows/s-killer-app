01634e67890390217d111054e17557aa9e05ac4c
joanna <joannawu@nlplab.cc>
2021-11-21 19:44:07 +0800

    spec: test SkillJob view object

--- spec/view_objects_spec.rb
+++ spec/view_objects_spec.rb
@@ -3,21 +3,17 @@
 require_relative 'helpers/vcr_helper'
 
 describe 'Test View Objects' do
-  Skiller::VcrHelper.setup_vcr
-
-  before do
-    Skiller::VcrHelper.configure_vcr_for_reed(REED_CASSETTE_FILE)
-  end
-
-  after do
-    Skiller::VcrHelper.eject_vcr
-  end
-
   describe 'Test Job Object' do
     before do
-      job_mapper = Skiller::Reed::JobMapper.new(CONFIG)
-      job_list = job_mapper.job_list(TEST_KEYWORD)
-      @job = job_mapper.job(job_list.first.job_id)
+      salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: nil)
+      @job = Skiller::Entity::Job.new(db_id: nil,
+                                      job_id: 0,
+                                      title: 'JOB TITLE',
+                                      description: '<h1>JOB TITLE</h1><p>description</p>',
+                                      location: 'LOCATION',
+                                      salary: salary,
+                                      url: 'URL',
+                                      is_full: true)
       @job_object = Views::Job.new(@job)
     end
 
@@ -37,7 +33,7 @@ describe 'Test View Objects' do
 
   describe 'Test Skill Object' do
     it 'HAPPY: should extract properties properly' do
-      salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: 'USD')
+      salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: nil)
       skill = Skiller::Entity::Skill.new(id: nil, 
                                          job_db_id: nil, 
                                          name: 'Python', 
@@ -83,15 +79,71 @@ describe 'Test View Objects' do
 
   describe 'Test SkillJob Object' do
     it 'HAPPY: should extract the skillset as skill object' do
+      salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: nil)
+      skills = [
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'AWS', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'Python', salary: salary)
+      ]
+      skilljob = Views::SkillJob.new(nil,  skills)
+      skilljob.skillset.each do |skill|
+        _(skill).must_be_instance_of Views::Skill
+      end
     end
 
     it 'HAPPY: should sort the skillset by count' do
+      salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: nil)
+      skills = [
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'AWS', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'Python', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'AWS', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'Python', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'JavaScript', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'Python', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'JavaScript', salary: salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'JavaScript', salary: salary)
+      ]
+      skilljob = Views::SkillJob.new(nil,  skills)
+      count = skilljob.skillset.map { |skill| skill.count }
+      _(count).must_equal count.sort.reverse!
     end
 
     it 'HAPPY: should return the job object' do
+      salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: nil)
+      jobs = [Skiller::Entity::Job.new(db_id: nil,
+                                       job_id: 0,
+                                       title: 'JOB1 TITLE',
+                                       description: '<h1>JOB1 TITLE</h1><p>description</p>',
+                                       location: 'LOCATION',
+                                       salary: salary,
+                                       url: 'URL',
+                                       is_full: true),
+              Skiller::Entity::Job.new(db_id: nil,
+                                       job_id: 1,
+                                       title: 'JOB2 TITLE',
+                                       description: '<h1>JOB2 TITLE</h1><p>description</p>',
+                                       location: 'LOCATION',
+                                       salary: salary,
+                                       url: 'URL',
+                                       is_full: true)]
+
+      skilljob = Views::SkillJob.new(jobs, nil)
+      skilljob.jobs.each do |job|
+        _(job).must_be_instance_of Views::Job
+      end
     end
 
     it 'HAPPY: should calculate the max/min salary' do
+      max_salary = Skiller::Value::Salary.new(year_min: 10.0, year_max: 1000.0, currency: 'USD')
+      min_salary = Skiller::Value::Salary.new(year_min: 1.0, year_max: 100.0, currency: 'USD')
+      nil_salary = Skiller::Value::Salary.new(year_min: nil, year_max: nil, currency: nil)
+      skills = [
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'Python', salary: nil_salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'Ruby', salary: max_salary),
+        Skiller::Entity::Skill.new(id: nil, job_db_id: nil, name: 'JavaScript', salary: min_salary)
+      ]
+      skilljob = Views::SkillJob.new(nil,  skills)
+      _(skilljob.max_salary).must_equal max_salary.year_max.to_i
+      _(skilljob.min_salary).must_equal min_salary.year_min.to_i
     end
   end
 end
