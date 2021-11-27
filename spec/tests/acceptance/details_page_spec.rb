f38c08dcb36ed9e9e3f6ebbd1715fe11c5b7e59b
Simon Lee <simon2020291@gmail.com>
2021-11-27 09:44:07 +0800

    refactor: use page object `DetailsPage` to perform the  acceptance tests of details page

--- spec/tests/acceptance/details_page_spec.rb
+++ spec/tests/acceptance/details_page_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'webdrivers/chromedriver'
-require 'watir'
-require 'minitest/autorun'
-require_relative '../../spec_helper'
+# require 'minitest/autorun'
+require_relative '../../helpers/acceptance_helper.rb'
+require_relative 'pages/details_page'
 
 
 describe 'Details Page Acceptance Tests' do
+  include PageObject::PageFactory
   before do
     @browser ||= Watir::Browser.new :chrome
   end
@@ -20,28 +20,30 @@ describe 'Details Page Acceptance Tests' do
   required_skills = %w[TypeScript AWS JavaScript]
   job_title_url = job_title.sub(' ', '+')
 
-  index_url = 'http://127.0.0.1:4001/'
+  index_url = CONFIG.APP_HOST
   # details_url = "http://127.0.0.1:4001/details?query=#{job_title.sub! ' ', '+'}"
-  details_url = "http://127.0.0.1:4001/details?query=#{job_title_url}"
+  details_url = "#{index_url}/details?query=#{job_title_url}"
 
   it '(HAPPY) should be able to read job details' do
     # Given: details page
-    @browser.goto details_url
+    visit(DetailsPage, using_params: { query: job_title_url }) do |page|
+      # When: user wants to see the details of a job
+      # User is able to see the title
+      _(page.title_element.present?).must_equal true
+      _(page.skill_list_element.present?).must_equal true
+      _(page.salary_list_element.present?).must_equal true
+      _(page.vacancies_element.present?).must_equal true
 
-    # When: user wants to see the details of a job
-    # User is able to see the title
-    @browser.span(id: 'title').present?
-    @browser.ul(id: 'skill_list').present?
-    @browser.ul(id: 'salary_list').present?
-    @browser.div(id: 'vacancies').present?
-    # Skills are properly presented
-    skill_list = @browser.ul(id: 'skill_list')
-    skills = skill_list.spans(id: 'skill_name').each(&:text)
-    required_skills.map { |required_skill| skills.include? required_skill }
+      # Skills are properly presented
+      skills = page.skills
+      required_skills.map do |required_skill|
+        _(skills.include? required_skill).must_equal true
+      end
 
-    # Then: user clicked search for other jobs
-    # Other links be tested here in future development
-    @browser.element(tag_name: 'a').click
-    _(@browser.url).must_match index_url
+      # Then: user clicked search for other jobs
+      # Other links be tested here in future development
+      page.return_to_index
+      _(@browser.url).must_match index_url
+    end
   end
 end
---
+++ spec/tests/acceptance/pages/details_page.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Page object for index page
+class DetailsPage
+  include PageObject
+
+  page_url Skiller::App.config.APP_HOST +
+           '/details?query=<%=params[:query]%>'
+
+  div(:warning_message, id: 'flash_bar_danger')
+  div(:success_message, id: 'flash_bar_success')
+
+  span(:title, id: 'title')
+  unordered_list(:skill_list, id: 'skill_list')
+  unordered_list(:salary_list, id: 'salary_list')
+  div(:vacancies, id: 'vacancies')
+  link(:to_last_page, id: 'to_last_page')
+
+  def skills
+    skill_list_element.spans(id: 'skill_name').map(&:text)
+  end
+
+  def return_to_index
+    to_last_page
+  end
+end
