709e6c650f8d6c486819fc6947b8d95756d39508
joanna <joannawu@nlplab.cc>
2021-11-28 17:38:15 +0800

    fix: store query_jobs only after skills are extracted

--- app/application/services/analyze_skills.rb
+++ app/application/services/analyze_skills.rb
@@ -12,13 +12,12 @@ module Skiller
       step :collect_jobs
       step :collect_skills
       step :calculate_salary_distribution
-      # step :analyze_skillset
+      step :store_query_to_db
 
       private
 
       # check if the previous form validation passes
       def parse_request(input)
-        puts 'step 1'
         if input.success?
           Success(query: input[:query])
         else
@@ -29,7 +28,6 @@ module Skiller
       # Collect jobs from database if the query has been searched;
       # otherwise, the entites will be created by mappers stored into the database
       def collect_jobs(input)
-        puts 'step 2'
         query = input[:query]
         if Repository::QueriesJobs.query_exist?(query)
           input[:jobs] = Repository::QueriesJobs.find_jobs_by_query(query)
@@ -44,7 +42,6 @@ module Skiller
       # Collect skills from database if the query has been searched;
       # otherwise, the entities will be created by mappers and stored into the database
       def collect_skills(input)
-        puts 'step 3'
         query = input[:query]
         if Repository::QueriesJobs.query_exist?(query)
           input[:skills] = Repository::QueriesJobs.find_skills_by_query(query)
@@ -58,7 +55,6 @@ module Skiller
 
       # Analyze the salary distribution from all related jobs
       def calculate_salary_distribution(input)
-        puts 'step 4'
         all_salary = input[:jobs].map(&:salary)
         input[:salary_dist] = Entity::SalaryDistribution.new(all_salary)
         Success(input)
@@ -66,18 +62,17 @@ module Skiller
         Failure(error.to_s)
       end
 
-      # def analyze_skillset(input)
-      #   skill_list = input[:skills]
-      #   skill_count = skill_list.group_by(&:name).transform_values(&:length)
-      #   skill_count.sort_by { |_, count| count }
-      #              .reverse!
-      #   sorted_skill.map do |name, count|
-      #     first_skill = skill_list.find { |skill| skill.name == name }
-      #     Views::Skill.new(first_skill, count)
-      #   end
-      # rescue StandardError => error
-      #   Failure(error.to_s)
-      # end
+      # Store the query-job
+      # Note that this MUST be executed as the last step,
+      #   when the jobs and skills are all correctly extracted,
+      #   or the skills of new jobs will not be analyzed forever
+      def store_query_to_db(input)
+        Repository::QueriesJobs.find_or_create(input[:query], 
+                                               input[:jobs].map(&:db_id))
+        Success(input)
+      rescue StandardError => error
+        Failure(error.to_s)
+      end
 
       # ------ UTILITIES ------ #
 
@@ -86,12 +81,10 @@ module Skiller
         job_mapper = Skiller::Reed::JobMapper.new(App.config)
         jobs = job_mapper.job_list(query)
         # [ TODO ] analyze skillset from more data
-        jobs = jobs[..10].map do |job|
+        jobs[..10].map do |job|
           full_job = job_mapper.job(job.job_id)
           Repository::Jobs.find_or_create(full_job)
         end
-        Repository::QueriesJobs.find_or_create(query, jobs.map(&:db_id))
-        jobs
       end
 
       # analyze the jobs' required skills from mapper and store into the database
