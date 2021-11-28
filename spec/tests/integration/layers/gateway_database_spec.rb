ba8e5177cfdbb0ed2ffc00f4f8dd6aafe94ad314
joanna <joannawu@nlplab.cc>
2021-11-28 15:11:58 +0800

    refactor: re-arrange folder structure

--- spec/tests/integration/layers/gateway_database_spec.rb
+++ spec/tests/integration/layers/gateway_database_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../../helpers/vcr_helper'
-require_relative '../../helpers/database_helper'
-require_relative '../../spec_helper'
+require_relative '../../../helpers/vcr_helper'
+require_relative '../../../helpers/database_helper'
+require_relative '../../../spec_helper'
 
 describe 'Integration Tests of Reed API and Database' do
   Skiller::VcrHelper.setup_vcr
